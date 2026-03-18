51b2f1c486a67aab2ba71b207dd1eb6c8ba686f3
rajeshwari-gith <galugur@mail.gvsu.edu>
2026-03-18 19:04:33 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <Beverage :isIced="currentTemp === 'Cold'" />
-    <ul>
-      <li>
-        <template v-for="temp in temps" :key="temp">
+    <ul class="option-row">
+      <li v-for="temp in temps" :key="temp"  class="align-item">
           <label>
             <input
               type="radio"
@@ -14,7 +13,48 @@
             />
             {{ temp }}
           </label>
-        </template>
+      </li>
+    </ul>
+    <ul class="option-row">
+      <li v-for="base in bases" :key="base.id" class="align-item">
+          <label>
+            <input
+              type="radio"
+              name="bases"
+              :id="`b-${base.id}`"
+              :value="base"
+              v-model="currentBase"
+            />
+            {{ base.name }}
+          </label>
+      </li>
+    </ul>
+    <ul class="option-row">
+      <li v-for="syrup in syrups" :key="syrup.id" class="align-item">
+          <label>
+            <input
+              type="radio"
+              name="syrups"
+              :id="`s-${syrup.id}`"
+              :value="syrup"
+              v-model="currentSyrup"
+            />
+            {{ syrup.name }}
+          </label>
+      </li>
+    </ul>
+    <ul class="option-row">
+      <li v-for="creamer in creamers" :key="creamer.id" class="align-item">
+          <label>
+            <input
+              type="radio"
+              name="creamers"
+              :id="`c-${creamer.id}`"
+              :value="creamer"
+              v-model="currentCreamer"
+            />
+            {{ creamer.name }}
+          </label>
       </li>
     </ul>
   </div>
@@ -22,7 +62,7 @@
 
 <script setup lang="ts">
 import Beverage from "./components/Beverage.vue";
-import { temps, currentTemp } from "./stores/beverage";
+import { temps, currentTemp, bases, currentBase, creamers, currentCreamer, syrups, currentSyrup } from "./stores/beverage";
 </script>
 
 <style lang="scss">
@@ -36,7 +76,24 @@ html {
   background-color: #6e4228;
   background: linear-gradient(to bottom, #6e4228 0%, #956f5a 100%);
 }
+
 ul {
   list-style: none;
+}
+
+.option-row {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0;
+}
+
+.align-item {
+  width: 120px;
+}
+
+.align-item label {
+  display: flex;
+  align-items: center;
+  gap: 0.35rem;
 }
 </style>
