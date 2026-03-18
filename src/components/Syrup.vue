e7761ba0156cd9b5150c3589edf2faf0beb78f39
rajeshwari-gith <galugur@mail.gvsu.edu>
2026-03-18 18:57:26 -0400

    Update Syrup.vue

--- src/components/Syrup.vue
+++ src/components/Syrup.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="syrup"></div>
+  <div class="syrup" :style="{ '--syrup-color': currentSyrup.color }"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { currentSyrup } from "../stores/beverage";
+</script>
+
 <style lang="scss" scoped>
+
+/*from assn2*/
+@mixin syrup-layer($color) {
+  background: repeating-linear-gradient(
+    45deg,
+    tan,
+    tan 10px,
+    $color 10px,
+    $color 20px
+  );
+}
+
 .syrup {
   transform: translateY(400%);
-  background-color: #c6c6c6;
+  @include syrup-layer(var(--syrup-color, #ffefd5));
   position: relative;
   width: 100%;
   height: 20%;
