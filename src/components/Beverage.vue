0cf6ed54d47782b9fa470c67867c95b38c460804
rajeshwari-gith <galugur@mail.gvsu.edu>
2026-03-18 18:56:41 -0400

    Update Beverage.vue

--- src/components/Beverage.vue
+++ src/components/Beverage.vue
@@ -4,10 +4,10 @@
     <Hot v-else />
     <Contents>
       <template v-slot:top>
-        <Creamer />
+        <Creamer v-if="!noCreamer" />
       </template>
       <template v-slot:mid>
-        <Syrup />
+        <Syrup v-if="!noSyrup" />
       </template>
       <template v-slot:bottom>
         <Base />
@@ -15,17 +15,25 @@
     </Contents>
   </Mug>
 </template>
+
 <script setup lang="ts">
+import Hot      from "./Hot.vue";
+import Mug      from "./Mug.vue";
+import Base     from "./Base.vue";
+import Cold     from "./Cold.vue";
+import Syrup    from "./Syrup.vue";
+import Creamer  from "./Creamer.vue";
 import Contents from "./Contents.vue";
-import Mug from "./Mug.vue";
-import Syrup from "./Syrup.vue";
-import Base from "./Base.vue";
-import Creamer from "./Creamer.vue";
-import Hot from "./Hot.vue";
-import Cold from "./Cold.vue";
+
+import { computed } from "vue";
+import { currentCreamer, currentSyrup } from "../stores/beverage";
 
 type Props = {
   isIced: boolean;
 };
 defineProps<Props>();
+
+const noCreamer = computed(() => currentCreamer.value.name === "No Creamer");
+const noSyrup = computed(() => currentSyrup.value.name === "No Syrup");
+
 </script>
